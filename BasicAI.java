bfc4a26297a559b7d755e1059e75ba213e53276b
noricchiii <74406287+noricchiii@users.noreply.github.com>
2021-01-11 21:58:17 +0800

    Add files via upload

---
+++ BasicAI.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class BasicAI extends AI {
+    public BasicAI () {
+        super();
+    }
+
+    /**
+     * This method randomly chooses a position in the board.
+     * @param board the given board configuration
+     */
+    public void algorithm (char[][] board) {
+        // get all the available slots first
+        int[][] available = super.getAvailableSlots(board);
+        
+        // generate a random number for the AI's next move
+        Random random = new Random();
+        int num = random.nextInt(available.length); 
+        
+        // set bestPosition to the coordinates of the random position
+        bestPosition[0] = available[num][0];
+        bestPosition[1] = available[num][1];
+    }
+}
